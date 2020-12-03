20c897d44edb6bb9faa281b0f77cae012c88b7e1
Bart Kalanski <bkalanski@gmail.com>
2020-12-03 19:04:29 +0000

    Laid out app structure

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Input from './Input'
+import List from './List'
+
+const App = () => {
+    return (
+       <div className="ui container">
+           <Input/>
+           <List/>
+       </div>
+    )
+}
+
+export default App
