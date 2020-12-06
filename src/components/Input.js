0de17bf1343e7ff6445e52f35da6e7b5021c0e37
Bart Kalanski <bkalanski@gmail.com>
2020-12-06 22:02:57 +0000

    Fixed empty entry bug

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 
 const Input = () => {
   const dispatch = useDispatch();
-  const destinations = useSelector((state) => state.destinations);
   const onFormSubmit = (event) => {
     event.preventDefault();
-    const value = event.target[0].value;
-    dispatch({ type: "ADD_DESTINATION", payload: value });
+    let value = event.target[0].value;
+    if (value) {
+      dispatch({ type: "ADD_DESTINATION", payload: value });
+    }
   };
   return (
     <React.Fragment>
